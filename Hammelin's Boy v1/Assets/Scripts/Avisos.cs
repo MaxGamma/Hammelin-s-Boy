8eef28a43976b8b39b8c467bb894689eec06dd07
Pau Masip <pau.masip@enti.cat>
2018-05-02 13:31:17 +0200

    Prueba merge with layouts

--- Hammelin's Boy v1/Assets/Scripts/Avisos.cs
+++ Hammelin's Boy v1/Assets/Scripts/Avisos.cs
@@ -21,7 +21,7 @@ public class Avisos : MonoBehaviour {
 
     void  OnCollisionEnter2D(Collision2D coll)
     {
-        if (coll.gameObject.tag == "Player")
+        if (coll.gameObject.tag == "Player" || coll.gameObject.tag == "Rat")
         {
             gameObject.GetComponent<BoxCollider2D>().enabled = false;
             animator.SetBool("isTrigger", true);
--- Hammelin's Boy v1/Assets/Scripts/PlayerMovement.cs
+++ Hammelin's Boy v1/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    /*
     //GameObject Player;
     public float alturaSalto;
     public float velocidadMovimiento;
@@ -60,4 +61,69 @@ public class PlayerMovement : MonoBehaviour
          alturaSalto);
         tierra = false;
     }
+}*/
+
+
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+   
+
+    private Rigidbody2D theRB;
+
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    public bool isGrounded;
+
+    private Animator anim;
+
+
+    
+
+	// Use this for initialization
+	void Start () {
+        theRB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+
+		if (Input.GetKey(left))
+        {
+            theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+        }
+        else if (Input.GetKey(right))
+        {
+            theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+        }
+        else
+        {
+            theRB.velocity = new Vector2(0, theRB.velocity.y);
+        }
+        if (Input.GetKey(jump) && isGrounded)
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+
+       
+        if (theRB.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-0.5f, 0.4f);
+        }
+        else if (theRB.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(0.5f, 0.4f);
+        }
+
+        //anim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
+        //anim.SetBool("Grounded", isGrounded);
+	}
 }
