a651d99f780a2c0f61498885b70cd5136dce9098
Pau Masip <pau.masip@enti.cat>
2018-05-07 11:48:16 +0200

    Big Update
    Max
    - GitKraken Working
    Sergi
    - Player doesn't get into the floor
    - Player doesn't get stuck in the walls
    Pau
    - Player doesn't get into the floor
    - Player doesn't get stuck in the walls
    - New maps working
    - Platforms animation implemented
    - Rat Idle anim working
    - UI works again
    - Changes on the spikes (now the script is on the player)
    - First enemy prototype working

--- Hammelin's Boy v1/Assets/Scripts/PlayerMovement.cs
+++ Hammelin's Boy v1/Assets/Scripts/PlayerMovement.cs
@@ -3,127 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class PlayerMovement : MonoBehaviour
-{
-    /*
+public class PlayerMovement : MonoBehaviour {
     //GameObject Player;
-    public float alturaSalto;
-    public float velocidadMovimiento;
+    public int nSaltos = 0;
     bool tierra = false;
     // Use this for initialization
-    void Start()
-    {
-        //  Player = GameObject.Find("Player");
-    }
+    void Start () {
+     //  Player = GameObject.Find("Player");
+	}
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Ground")
         {
-
+            Debug.Log("Tierra True");
             tierra = true;
         }
     }
     // Update is called once per frame
-    void FixedUpdate()
-    {
-
-        if (Input.GetKey(KeyCode.Space) && (tierra == true))
+    void FixedUpdate () {
+      
+            if (Input.GetKey(KeyCode.Space))
+            {
+                tierra = false;
+                   GetComponent<Rigidbody2D>().velocity = new Vector2(0, 5);
+                
+            }
+            
+            if (Input.GetKey(KeyCode.D))
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(20, 0));
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -20));
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(-20, 0));
+            }
+        //}
+       /*else
         {
-            Saltar();
-
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.localScale = new Vector3(0.4f, 0.3f);
-            GetComponent<Rigidbody2D>().velocity = new Vector2(velocidadMovimiento, GetComponent<Rigidbody2D>().velocity.y);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.localScale = new Vector3(-0.4f, 0.3f);
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-velocidadMovimiento, GetComponent<Rigidbody2D>().velocity.y);
-        }
-        if (Input.GetKeyUp(KeyCode.D))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-        }
-
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
-        }
-
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0,-10));
+ 
+            saltar = false;
+        }*/
     }
-    public void Saltar()
-    {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x,
-         alturaSalto);
-        tierra = false;
-    }
-}*/
-
-
-
-    public float moveSpeed;
-    public float jumpForce;
-
-    public KeyCode left;
-    public KeyCode right;
-    public KeyCode jump;
    
-
-    private Rigidbody2D theRB;
-
-    public Transform groundCheckPoint;
-    public float groundCheckRadius;
-    public LayerMask whatIsGround;
-
-    public bool isGrounded;
-
-    private Animator anim;
-
-
-    
-
-	// Use this for initialization
-	void Start () {
-        theRB = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
-
-		if (Input.GetKey(left))
-        {
-            theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
-        }
-        else if (Input.GetKey(right))
-        {
-            theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
-        }
-        else
-        {
-            theRB.velocity = new Vector2(0, theRB.velocity.y);
-        }
-        if (Input.GetKey(jump) && isGrounded)
-        {
-            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
-        }
-
-       
-        if (theRB.velocity.x < 0)
-        {
-            transform.localScale = new Vector3(-0.5f, 0.4f);
-        }
-        else if (theRB.velocity.x > 0)
-        {
-            transform.localScale = new Vector3(0.5f, 0.4f);
-        }
-
-        //anim.SetFloat("speed", Mathf.Abs(theRB.velocity.x));
-        //anim.SetBool("Grounded", isGrounded);
-	}
 }
+
+
